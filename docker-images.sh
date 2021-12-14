85dacf379b1bcfbff9c3954480395d67a9f84bae
arifbudimanar <arifbudiman01041996@hotmail.com>
2021-12-14 13:44:14 +0700

    initial commit

---
+++ docker-images.sh
@@ -0,0 +1,8 @@
+# untuk melihat images yang ada di docker
+docker image ls
+# untuk download docker images
+docker image pull image:tag
+# untuk menghapus docker images
+docker image rm image:tag
+# untuk mengambil informasi dari docker images
+docker image inspect image:tag
